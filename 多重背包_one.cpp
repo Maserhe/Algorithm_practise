d8528b0749948b2155d6504f230de6320137e498
Maserhe <49369068+Maserhe@users.noreply.github.com>
2020-04-25 21:50:35 +0800

    朴素算法

--- 多重背包_one.cpp
+++ 多重背包_one.cpp
@@ -2,24 +2,24 @@
 #include<algorithm>
 using namespace std;
 
-const int N = 1010;
+const int N = 110;
 
-int f[N][N],n,m;
+int f[N][N];
 
-int v[N], w[N];
+int n , m ; //这是朴素算法
+int v[N], w[N], k[N];  //容积 ， 权重 ， 最大数量 
+
 int main(){
     
-    cin>>n>>m;
-    
-    for(int i = 1 ; i <= n ; i ++ ) cin>>v[i]>>w[i];
+    scanf("%d%d",&n,&m);
+    for(int i = 1 ; i <= n ; i ++ ) scanf("%d%d%d", &v[i],&w[i],&k[i]);
     
     for(int i =1 ; i <= n ; i ++ )
-        for(int j = 0 ; j <= m ; j ++ )
-            for(int k = 0 ; k*v[i] <= j ; k ++ )
-                f[i][j] = max(f[i][j], f[i -1][j - v[i]*k] + k* w[i]); 
+        for(int j  = 0 ; j <= m ; j ++ )
+            for(int s = 0 ; s <= k[i]&& s*v[i]<= j ; s++ )
+                f[i][j] = max(f[i][j], f[i - 1 ][ j - s* v[i]] + s*w[i] );
     
     cout<<f[n][m]<<endl;
     
-    system("pause");
     return 0;
 }

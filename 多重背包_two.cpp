d12ca431f0321615d8f2be2359c5bdb888f6d6a3
Maserhe <49369068+Maserhe@users.noreply.github.com>
2020-04-25 22:42:02 +0800

    二进制优化

--- 多重背包_two.cpp
+++ 多重背包_two.cpp
@@ -1,22 +1,36 @@
 #include<iostream>
 #include<algorithm>
-using namespace std;
+using namespace std; //将多重背包 划分成  01背包
 
-const int N = 1010;
+const int N =  25000;   //使用二进制进行优化  
 
-int f[N][N];
-int n , m;
-int v[N],w[N];
-int main(){
+int f[N], n ,m  ;
+int v[N], w[N];
+int main(){ 
+    
     cin>>n>>m;
-
-    for(int i = 1 ; i<= n ; i ++ ) cin>>v[i]>>w[i];
-
-    for(int i = 1 ; i <= n ; i ++ )
-        for( int j = 0; j <= m ; j ++ ) 
-            f[i][j] = max(f[i-1][j],f[i][j-v[i]]+w[i] );
-
-    cout<<f[n][m];
-    system("pause");
+    int cnt =0;
+    for(int i= 1 ; i <= n ; i ++ ){
+        int a, b ,s;
+        cin>>a>>b>>s;
+        int k = 1;
+        
+        while(k<=s){
+            cnt++;
+            v[cnt] = a*k;
+            w[cnt] = b*k;
+            s-= k;
+            k*=2;
+        }
+        if(s>0){
+            cnt++;
+            v[cnt] = a*s;
+            w[cnt] = b*s;
+        }
+    }
+    for( int i = 1 ; i <= cnt ; i ++ )
+        for(int j = m ; j >=v[i] ; j -- )
+            f[j] = max(f[j], f[j-v[i]]+w[i]);
+    cout<<f[m];
     return 0;
 }

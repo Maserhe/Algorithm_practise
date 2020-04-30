af114e21fcf23e810ea8386afb8a8cf731bd6f81
Maserhe <982289931@qq.com>
2020-04-30 23:18:36 +0800

    求约数

---
+++ 试除法求约数.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+//试除法求所有约数 , 约数 大于 等于1的. 素数是从二开始的
+vector<int> get_divisors(int n){
+    vector<int> res;
+    for( int  i = 1; i < n/i ; i ++ ){
+        if( n % i == 0 ) {
+            res.push_back(i);
+            if( i != n/i ) res.push_back(n/i); //i如果等于 n/i的话 只加一个
+        } 
+    }
+    sort(res.begin(),res.end());
+    return res;
+
+}
+int main( ) }{
+
+    int n;
+    cin>>n;
+    while ( n--)
+    {
+        int x ;
+        cin>> x;
+        auto t = get_divisors(x);
+        for( auto t : res) cout<<t<<" " endl;
+    }
+
+    return 0;
+}

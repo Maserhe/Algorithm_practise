0b681c929fa2136bfce2fe30f2d0d46d1579d479
Maserhe <982289931@qq.com>
2020-05-12 20:10:56 +0800

    练习

---
+++ 二维费用背包.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+const int N =100;
+int f[N][N] ;
+
+int main(){
+
+    int n , V ,W ;
+    cin>>n>>V>>W;
+    
+    for(int i = 0 ; i < n ; i ++ ){
+        int a , b , c;  //体积、重量和价值。
+        cin>>a>>b>>c;
+
+        for(int j = V; j >=a ; j--){
+            for(int k = W ; k>=b ; k--){
+
+                f[j][k] = max(f[j][k] , f[j-a ][k-b] + c);
+            }
+        }
+
+    }
+
+    cout<<f[V][W];
+    system("pause");
+    return 0;
+}
